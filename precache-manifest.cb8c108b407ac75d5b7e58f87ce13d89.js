4096e643748f1ebf35095706195f4e06752e648d
shivamtiwari101 <77287982+shivamtiwari101@users.noreply.github.com>
2021-09-09 23:37:03 +0530

    Updates

---
+++ precache-manifest.cb8c108b407ac75d5b7e58f87ce13d89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9c8113d8494cf2669a162ef53f019c",
+    "url": "/NewsUpdateDaly/index.html"
+  },
+  {
+    "revision": "79acb2c6e7798aa40070",
+    "url": "/NewsUpdateDaly/static/js/2.89cc5bf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewsUpdateDaly/static/js/2.89cc5bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a107ed0d6b8eb7d7a2b",
+    "url": "/NewsUpdateDaly/static/js/main.2f6ce3b2.chunk.js"
+  },
+  {
+    "revision": "db5663d22d6419293d56",
+    "url": "/NewsUpdateDaly/static/js/runtime-main.9f214766.js"
+  },
+  {
+    "revision": "86c83439179bd2025a26e39f280e96aa",
+    "url": "/NewsUpdateDaly/static/media/off.86c83439.svg"
+  },
+  {
+    "revision": "199f62c3ce2253ce6c64da0b2300469a",
+    "url": "/NewsUpdateDaly/static/media/speaker.199f62c3.svg"
+  }
+]);
